5c887654b90feb7aa6758a051db2c3388b356a31
anunyachowdary <anunyachowdary125@gmail.com>
2022-11-03 12:05:28 +0530

    first commit

--- models/user.js
+++ models/user.js
@@ -3,12 +3,16 @@ const validator = require('validator')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 
+
 const userSchema = new mongoose.Schema({
+
+
+
     name: {
        type: String,
        required: true,
        trim: true,
-       
+
      },
     email: {
        type: String,
@@ -21,15 +25,14 @@ const userSchema = new mongoose.Schema({
                      }
                 }
       },
-
-    password : {
+    password: {
         type: String,
         required: true,
         minLength: 7,
         trim: true,
         validate(value) {
            if( value.toLowerCase().includes('password')) {
-           throw new Error('Password musn\'t contain password')
+           throw new Error('password musnt contain password')
           }
        }
     },
@@ -43,34 +46,41 @@ const userSchema = new mongoose.Schema({
     timestamps: true
     })
 
+
     userSchema.methods.generateAuthToken = async function () {
       const user = this
-      const token =jwt.sign({_id: user._id.toString()}, 'jgjfhftkgjgj-hkjhkh')
+      const token = jwt.sign({ _id: user._id.toString()}, 'kdh988w7krgs-ijigujj')
       user.tokens = user.tokens.concat({token})
-      await user.save()
+       await user.save()
       return token
-    }
-//login in users
-userSchema.statics.findBycredentials = async (email,password) => {
-  const user = await User.findOne({ email })
-  if(!user) {
-    throw new Error('unable to login')
   }
-  const isMatch = await bcrypt.compare(password, user.password)
-  console.log(isMatch)
-  if(!isMatch) {
-    throw new Error('unable to login')
-  }
-  return user
-}
-userSchema.pre('save', async function(next) {
 
-  const user =this
-  if (user.isModified('password')){
-    user.password = await bcrypt.hash(user.password, 8)
+  //login in users
+  userSchema.statics.findByCredentials = async (email, password) => {
+      const user = await User.findOne({ email })
+      if (!user) {
+          throw new Error('Unable to log in')
+      }
+      const isMatch = await bcrypt.compare(password, user.password)
+      console.log(isMatch)
+      if(!isMatch) {
+          throw new Error('Unable to login')
+      }
+
+      return user
   }
-  next()
-})
+
+  //Hash plain password before saving
+  userSchema.pre('save', async function(next) {
+      const user = this
+      if (user.isModified('password')) {
+          user.password = await bcrypt.hash(user.password, 8)
+      }
+
+      next()
+  })
+
 
     const User = mongoose.model('User', userSchema)
-module.exports = User
+
+    module .exports= User
--- routes/userrouter.js
+++ routes/userrouter.js
@@ -4,34 +4,32 @@ const Auth = require('../middileware/auth')
 
 const router = new express.Router()
 
-//signup
+
+
+
+ //signup
 router.post('/users', async (req, res) => {
     const user = new User(req.body)
-
     try {
         await user.save()
         const token = await user.generateAuthToken()
         res.status(201).json({
             registeredUser:user
             , token})
-        } catch (error) {
+    } catch (error) {
         res.status(400).json(error)
-        console.log(error)
     }
-
-})
+    })
 
 //login
 
 router.post('/users/login', async (req, res) => {
-   
     try {
-        console.log('jhvjkdf')
         const user = await User.findByCredentials(req.body.email, req.body.password)
         console.log(user)
         const token = await user.generateAuthToken()
         console.log(token)
-        res.status(201).json({token})
+        res.status(201).json({user, token})
     } catch (error) {
         res.status(400).json(error)
     }
@@ -47,14 +45,14 @@ router.post('/users/logout', Auth, async (req, res) => {
 
         await req.user.save()
         res.status(200).json({
-            message:'logout'
+            message:'Logged out successfuly'
         })
     } catch (error) {
         res.status(500).send()
     }
 })
 
-//Logout All 
+/*//Logout All 
 router.post('/users/logoutAll', Auth, async(req, res) => {
     try {
         req.user.tokens = []
@@ -64,4 +62,6 @@ router.post('/users/logoutAll', Auth, async(req, res) => {
         res.status(500).send()        
     }
 })
-module.exports = router
+*/
+
+    module.exports=router
